2eb36ee362c1e3dc016c0c289647bcfe282b100f
ervnjmsdnts <earvinjamesdantes@gmail.com>
2021-05-17 18:01:27 +0800

    override update and destroy method in the views

--- api/views/employee/employee.py
+++ api/views/employee/employee.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.response import Response
 
 from api.models.employee.models import (
     Employee, Address, ContactPhone, ContactEmail, ContactSocialLinks, Education
@@ -13,22 +14,106 @@ class EmployeeViews(viewsets.ModelViewSet):
     serializer_class = EmployeeSerializers
     queryset = Employee.objects.all()
 
+    def destroy(self, request, *args, **kwargs):
+        item = self.get_object()
+        item.active_status = False
+        item.save()
+
+        return Response(data='not active')
+
+    def update(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(data=serializer.data)
+
 class AddressViews(viewsets.ModelViewSet):
     serializer_class = AddressSerializers
     queryset = Address.objects.all()
+
+    def destroy(self, request, *args, **kwargs):
+        item = self.get_object()
+        item.active_status = False
+        item.save()
+
+        return Response(data='not active')
+
+    def update(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(data=serializer.data)
 
 class ContactPhoneViews(viewsets.ModelViewSet):
     serializer_class = ContactPhoneSerializers
     queryset = ContactPhone.objects.all()
 
+    def destroy(self, request, *args, **kwargs):
+        item = self.get_object()
+        item.active_status = False
+        item.save()
+
+        return Response(data='not active')
+
+    def update(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(data=serializer.data)
+
 class ContactEmailViews(viewsets.ModelViewSet):
     serializer_class = ContactEmailSerializers
     queryset = ContactEmail.objects.all()
+
+    def destroy(self, request, *args, **kwargs):
+        item = self.get_object()
+        item.active_status = False
+        item.save()
+
+        return Response(data='not active')
+
+    def update(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(data=serializer.data)
 
 class ContactSocialLinksViews(viewsets.ModelViewSet):
     serializer_class = ContactSocialLinksSerializer
     queryset = ContactSocialLinks.objects.all()
 
+    def destroy(self, request, *args, **kwargs):
+        item = self.get_object()
+        item.active_status = False
+        item.save()
+
+        return Response(data='not active')
+
+    def update(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(data=serializer.data)
+
 class EducationViews(viewsets.ModelViewSet):
     serializer_class = EducationSerializers
     queryset = Education.objects.all()
+
+    def destroy(self, request, *args, **kwargs):
+        item = self.get_object()
+        item.active_status = False
+        item.save()
+
+        return Response(data='not active')
+
+    def update(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(data=serializer.data)
